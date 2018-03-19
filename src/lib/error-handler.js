567536bdc912d8ada20be7ddd549a4a90c1bcafd
Rima <rmhrok@gmail.com>
2018-03-19 22:54:27 +0000

    add travis/fix linter errors

--- src/lib/error-handler.js
+++ src/lib/error-handler.js
@@ -5,8 +5,6 @@ module.exports = function(err, res) {
   let errMsg = `${err.name}: ${err.message}`;
   let msg = errMsg.toLowerCase();
 
-  console.log('error msg', msg);
-
   switch(true) {
   case  msg.includes('path error'): return res.status(404).send(errMsg);
   default: return res.status(500).send(errMsg);
--- src/route/clips.js
+++ src/route/clips.js
@@ -7,10 +7,10 @@ const clientId = process.env.TWITCH_CLIENT_ID;
 module.exports = function(router) {
   router.route('/clips/top')
     .get((req, res) => {
-        superagent.get('https://api.twitch.tv/kraken/clips/top?limit=10')
+      superagent.get('https://api.twitch.tv/kraken/clips/top?limit=10')
         .set('Client-ID', clientId)
         .set('Accept', 'application/vnd.twitchtv.v5+json')
         .then(response => res.send(response.body))
-        .catch(err => errorHandler(err, res))
-    })
+        .catch(err => errorHandler(err, res));
+    });
 };
