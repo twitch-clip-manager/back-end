0eef74093f94aea84c42b6ddcf86c4419f5f4da1
billodell <billodell5@gmail.com>
2018-03-19 14:25:33 -0700

    game query working

--- src/route/route.js
+++ src/route/route.js
@@ -4,8 +4,16 @@ const superagent = require('superagent')
 module.exports = function(router) {
   router.route('/clips/top')
     .get((req, res) => {
-        console.log('hello')
         superagent.get('https://api.twitch.tv/kraken/clips/top?limit=10')
+        .set('Client-ID', '490xfzohxg3ilxjcq7uifjssyqwo5e')
+        .set('Accept', 'application/vnd.twitchtv.v5+json')
+        .then(response => res.send(response.body))
+        .catch(err => console.log(err.message))
+    })
+  router.route('/clips/game:id')
+    .get((req, res) => {
+        console.log('req.params',req.params)
+        superagent.get(`https://api.twitch.tv/kraken/clips/top?limit=10&game=${req.params.id}`)
         .set('Client-ID', '490xfzohxg3ilxjcq7uifjssyqwo5e')
         .set('Accept', 'application/vnd.twitchtv.v5+json')
         .then(response => res.send(response.body))
