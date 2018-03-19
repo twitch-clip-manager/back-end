b758f8feb669335b8f38d869bd5f0e206f0569d7
Rima <rmhrok@gmail.com>
2018-03-19 21:26:39 +0000

    create .env file

--- src/route/clips.js
+++ src/route/clips.js
@@ -2,12 +2,13 @@
 
 const errorHandler = require('../../src/lib/error-handler');
 const superagent = require('superagent');
+const clientId = process.env.TWITCH_CLIENT_ID;
 
 module.exports = function(router) {
   router.route('/clips/top')
     .get((req, res) => {
         superagent.get('https://api.twitch.tv/kraken/clips/top?limit=10')
-        .set('Client-ID', '490xfzohxg3ilxjcq7uifjssyqwo5e')
+        .set('Client-ID', clientId)
         .set('Accept', 'application/vnd.twitchtv.v5+json')
         .then(response => res.send(response.body))
         .catch(err => errorHandler(err, res))
