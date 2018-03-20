2048ab176eaf57c59c34a71cea7ce965493e630d
Rima <rmhrok@gmail.com>
2018-03-20 20:24:38 +0000

    tests game/channel routes

--- __test__/integration-test/get-clips.test.js
+++ __test__/integration-test/get-clips.test.js
@@ -3,6 +3,8 @@
 const server = require('../../src/lib/server');
 const superagent = require('superagent');
 const PORT = process.env.PORT;
+const GAME_KEYWORD = 'overwatch';
+const CHANNEL_KEYWORD = 'admiralbulldog';
 
 describe('GET', function() {
 
@@ -33,6 +35,12 @@ describe('GET', function() {
         });
 
       test(
+        'should have embed html in each clip object',
+        () => {
+          expect(topTen.clips[0]).toHaveProperty('embed_html');
+        });
+
+      test(
         'should return in instance of object',
         () => {
           expect(topTen instanceof Object).toBe(true);
@@ -55,7 +63,93 @@ describe('GET', function() {
         });
 
     });
-
   });
 
+  describe('GET /clips/game:id', function () {
+
+    let gameTen;
+    // get top ten clips for a specific game
+    beforeAll(() => {
+      return superagent.get(`:${PORT}/clips/game${GAME_KEYWORD}`)
+        .then(res => gameTen = res.body)
+    });
+
+    describe('Valid input', () => {
+
+      test(
+        'should have ten clips objects in an array',
+        () => {
+          expect(gameTen.clips.length).toEqual(10);
+        });
+
+      test(
+        'should have embed url in each clip object',
+        () => {
+          expect(gameTen.clips[0]).toHaveProperty('embed_url');
+        });
+
+      test(
+        'should have embed html in each clip object',
+        () => {
+          expect(gameTen.clips[0]).toHaveProperty('embed_html');
+        });
+
+      test(
+        'should return in instance of object',
+        () => {
+          expect(gameTen instanceof Object).toBe(true);
+        });
+
+      test(
+        'should have array with key "clips"',
+        () => {
+          expect(gameTen.clips instanceof Array).toBe(true);
+        });
+
+    });
+  });
+
+  describe('GET /clips/channel:id', function () {
+
+    let channelTen;
+    // get top ten clips for a specific channel
+    beforeAll(() => {
+      return superagent.get(`:${PORT}/clips/channel${CHANNEL_KEYWORD}`)
+        .then(res => channelTen = res.body)
+    });
+
+    describe('Valid input', () => {
+
+      test(
+        'should have ten clips objects in an array',
+        () => {
+          expect(channelTen.clips.length).toEqual(10);
+        });
+
+      test(
+        'should have embed url in each clip object',
+        () => {
+          expect(channelTen.clips[0]).toHaveProperty('embed_url');
+        });
+
+      test(
+        'should have embed html in each clip object',
+        () => {
+          expect(channelTen.clips[0]).toHaveProperty('embed_html');
+        });
+
+      test(
+        'should return in instance of object',
+        () => {
+          expect(channelTen instanceof Object).toBe(true);
+        });
+
+      test(
+        'should have array with key "clips"',
+        () => {
+          expect(channelTen.clips instanceof Array).toBe(true);
+        });
+
+    });
+  });
 });
